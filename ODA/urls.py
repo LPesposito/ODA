fb7c5fca03d7e165ab7b3a638199f2cd65d92fb9
Luan Esposito <luanesposito2003@gmail.com>
2024-10-12 13:41:48 -0300

    Adicionado Home e configuração static

--- ODA/urls.py
+++ ODA/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from ODA import views
 
-urlpatterns = [path("", views.home)]
+urlpatterns = [
+    path("", views.home),
+]
--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,9 @@
-
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include('ODA.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

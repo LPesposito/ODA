0334ca5348f6040006ee86ffaaf3d1df411781a3
Luan Esposito <luanesposito2003@gmail.com>
2024-10-14 11:30:53 -0300

    tryed

--- ODA/urls.py
+++ ODA/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from ODA import views
 
 urlpatterns = [
     path("", views.home),
     path("atividade/lab", views.ativ_lab, name="Virtual Lab"),
-    path("teste", views.test_template, name="Teste!")
+    path("teste", views.test_template, name="Teste!"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="login/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="login/logout.html"),
+        name="logout",
+    ),
 ]

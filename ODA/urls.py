e16f345d607d631d81fcb22223a8f63b35ebc911
Luan Esposito <luanesposito2003@gmail.com>
2024-10-15 18:46:36 -0300

    atividades.html

--- ODA/urls.py
+++ ODA/urls.py
@@ -4,7 +4,7 @@ from ODA import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("atividades/lab", views.ativ_lab, name="virtual_lab"),
+    path("atividades/lab", views.ativ_lab, name="lab"),
     path("atividades", views.atividades, name="atividades"),
     path("teste", views.test_template, name="teste"),
 ]
